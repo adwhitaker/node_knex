ac9d61cce77c2907fb36442748f55b371d3fd5df
Alex Whitaker <whitaker.alexander@gmail.com>
2017-10-25 15:09:42 -0500

    move routes config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var initRoutes = require('./routes/routes-config');
+var initRoutes = require('./config/routes-config');
 
 const app = express();
 
