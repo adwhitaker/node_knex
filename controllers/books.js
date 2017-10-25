a789a543ff1e1d0647439ffd8a21129c8f1c07ac
Alex Whitaker <whitaker.alexander@gmail.com>
2017-10-25 15:29:35 -0500

    return books

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,13 @@
+var Book = require('../models/Book');
+var seeds = require('../db/seeds');
+
 function getBooks(req, res) {
-    res.send('respond with a resource');
+    let books = [];
+    seeds.books.forEach(book => {
+        const newBook = new Book(book.title, book.author, book.publisher, book.publish_year);
+        books.push(newBook);
+    });
+    res.send({ books });
 }
 
 module.exports = {
