e9f4b28a3d0b67b9b9da8ac4fd72352d96a03e5c
Alex Whitaker <whitaker.alexander@gmail.com>
2017-10-25 15:29:19 -0500

    book seeds

---
+++ db/seeds.js
@@ -0,0 +1,24 @@
+const books = [
+    {
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        publisher: 'J. B. Lippincott & Co.',
+        publish_year: 1960
+    },
+    {
+        title: '1984',
+        author: 'George Orwell',
+        publisher: 'Secker & Warburg',
+        publish_year: 1949
+    },
+    {
+        title: 'Harry Potter and the Sorcerer\'s Stone',
+        author: 'J. K. Rowling',
+        publisher: 'Bloomsbury',
+        publish_year: 1997
+    }
+]
+
+module.exports = {
+    books
+};
