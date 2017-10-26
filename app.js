ad7eed710729de2f2c0b03052f3876cda6088616
Alex Whitaker <whitaker.alexander@gmail.com>
2017-10-26 00:01:58 -0500

    update handlers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var initRoutes = require('./config/routes-config');
+var mountHandlers = require('./handlers/index');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-initRoutes(app);
+mountHandlers(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- handlers/users.js
+++
@@ -1,8 +0,0 @@
-const routes = require('../config/routes');
-const router = require('express').Router();
-const usersController = require('../controllers/users');
-
-router.route(routes.default)
-    .get(usersController.getUsers);
-
-module.exports = router;
